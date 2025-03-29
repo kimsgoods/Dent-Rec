74137f9cce4b410231a1128e9ad514cefe107880
Kim Escabarte <kimescabarte@gmail.com>
2025-03-29 09:45:38 +0800

    Add exception middleware and logging

---
+++ DentRec.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+
+namespace DentRec.API.Middleware
+{
+    public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+                await HandleException(context, ex);
+            }
+        }
+
+        private static Task HandleException(HttpContext context, Exception ex)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+            response.StatusCode = ex switch
+            {
+                ArgumentException => (int)HttpStatusCode.BadRequest, // 400
+                KeyNotFoundException => (int)HttpStatusCode.NotFound, // 404
+                UnauthorizedAccessException => (int)HttpStatusCode.Unauthorized, // 401
+                _ => (int)HttpStatusCode.InternalServerError // 500
+            };
+
+            var errorResponse = new { message = ex.Message, statusCode = response.StatusCode };
+            return response.WriteAsync(JsonSerializer.Serialize(errorResponse));
+        }
+    }
+}
--- DentRec.API/Program.cs
+++ DentRec.API/Program.cs
@@ -1,14 +1,23 @@
+using DentRec.API.Middleware;
+using DentRec.Application.Interfaces;
+using DentRec.Application.Services;
 using DentRec.Infrastructure;
+using DentRec.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IPatientService, PatientService>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,6 +25,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
