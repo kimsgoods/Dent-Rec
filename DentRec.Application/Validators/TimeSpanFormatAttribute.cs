f0a4480c0e5028b3cf5d2881073ad4e70d7b463e
Kim Escabarte <kimescabarte@gmail.com>
2025-03-31 12:20:08 +0800

    Add TimeSpanFormatAttribute validator

---
+++ DentRec.Application/Validators/TimeSpanFormatAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+public class TimeSpanFormatAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value is null) return true; // Allow null values (optional fields)
+
+        if (value is string timeString)
+        {
+            return TimeSpan.TryParseExact(timeString, @"hh\:mm\:ss", CultureInfo.InvariantCulture, out _);
+        }
+
+        return false;
+    }
+
+    public override string FormatErrorMessage(string name)
+    {
+        return $"{name} must be in the format hh:mm:ss.";
+    }
+}
