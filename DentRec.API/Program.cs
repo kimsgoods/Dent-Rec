a459105273594d0237a8772e7b8a7f49392e8020
Kim Escabarte <kimescabarte@gmail.com>
2025-04-28 20:14:58 +0800

    Update Angular build configs

--- DentRec.API/Program.cs
+++ DentRec.API/Program.cs
@@ -44,8 +44,27 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseDefaultFiles();
+
+app.UseStaticFiles();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapFallbackToFile("index.html");
+
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    await context.Database.MigrateAsync();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    throw;
+}
 
 app.Run();
