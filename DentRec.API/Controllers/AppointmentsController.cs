e07130c9ecc46731fcfb7433ba80b7ed7eeff340
Kim Escabarte <kimescabarte@gmail.com>
2025-05-31 10:49:52 +0800

    Add CreateAppointmentCommand

---
+++ DentRec.API/Controllers/AppointmentsController.cs
@@ -0,0 +1,16 @@
+﻿using DentRec.Application.CQRS.Appointments.Commands.CreateAppointment;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DentRec.API.Controllers
+{
+    public class AppointmentsController(ISender sender) : BaseApiController
+    {
+        [HttpPost]
+        public async Task<ActionResult<int>> CreatePatientLog(CreateAppointmentCommand command)
+        {
+            var result = await sender.Send(command);
+            return result;
+        }
+    }
+}
---
+++ DentRec.Application/CQRS/Appointments/Commands/CreateAppointment/CreateAppointmentCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentRec.Application.CQRS.Appointments.Commands.CreateAppointment
+{
+    public class CreateAppointmentCommand : IRequest<int>
+    {
+        public int PatientId { get; set; }
+        public int DentistId { get; set; }
+        public DateTime AppointmentDateTime { get; set; }
+        public string? Notes { get; set; }
+    }
+
+}
